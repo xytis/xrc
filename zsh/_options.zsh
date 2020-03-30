caea5131740821296292535abda0f97f93b6c611
Vytis Valentinavičius <vytis@nolife4life.org>
2020-03-30 10:47:13 +0300

    Added zsh history on up/down keys

--- zsh/_options.zsh
+++ zsh/_options.zsh
@@ -4,3 +4,11 @@ SPROMPT='zsh: correct %F{red}%R%f to %F{green}%r%f [nyae]? '
 
 # Esure that vi mode is enabled
 set -o vi
+
+# Ensure zsh history is bound to up/down arrows
+bindkey '^[[A' history-substring-search-up
+bindkey '^[[B' history-substring-search-down
+bindkey "$terminfo[kcuu1]" history-substring-search-up
+bindkey "$terminfo[kcud1]" history-substring-search-down
+bindkey -M vicmd 'k' history-substring-search-up
+bindkey -M vicmd 'j' history-substring-search-down

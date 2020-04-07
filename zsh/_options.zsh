4a2f1d1fe29948dc0fcdfc32d38739f5c986db26
Vytis Valentinavičius <vytis@nolife4life.org>
2020-04-07 12:33:27 +0300

    Reverted to direct mac keybindings

--- zsh/_options.zsh
+++ zsh/_options.zsh
@@ -5,13 +5,10 @@ SPROMPT='zsh: correct %F{red}%R%f to %F{green}%r%f [nyae]? '
 # Esure that vi mode is enabled
 set -o vi
 
-# Load keybindings indiferent from terminal
-autoload zkbd
-
 # Ensure zsh history is bound to up/down arrows
-bindkey "${key[Up]}"    history-substring-search-up
-bindkey "${key[Down]}"  history-substring-search-down
+bindkey "$terminfo[kcuu1]"  history-substring-search-up
+bindkey "$terminfo[kcud1]"  history-substring-search-down
 
 # Ensure Home/End still work on a line in vi mode
-bindkey "${key[Home]}"  beginning-of-line
-bindkey "${key[End]}"   end-of-line
+bindkey "$terminfo[khome]"  beginning-of-line
+bindkey "$terminfo[kend]"   end-of-line

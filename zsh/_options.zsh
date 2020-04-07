6297898b4280f481def8968ba095fe0453aca134
Vytis Valentinavičius <vytis@nolife4life.org>
2020-04-07 12:07:11 +0300

    Fixed arrows in vi mode

--- zsh/_options.zsh
+++ zsh/_options.zsh
@@ -5,10 +5,13 @@ SPROMPT='zsh: correct %F{red}%R%f to %F{green}%r%f [nyae]? '
 # Esure that vi mode is enabled
 set -o vi
 
+# Load keybindings indiferent from terminal
+autoload zkbd
+
 # Ensure zsh history is bound to up/down arrows
-bindkey '^[[A' history-substring-search-up
-bindkey '^[[B' history-substring-search-down
-bindkey "$terminfo[kcuu1]" history-substring-search-up
-bindkey "$terminfo[kcud1]" history-substring-search-down
-bindkey -M vicmd 'k' history-substring-search-up
-bindkey -M vicmd 'j' history-substring-search-down
+bindkey "${key[Up]}"    history-substring-search-up
+bindkey "${key[Down]}"  history-substring-search-down
+
+# Ensure Home/End still work on a line in vi mode
+bindkey "${key[Home]}"  beginning-of-line
+bindkey "${key[End]}"   end-of-line

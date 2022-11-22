cd4bef1e847a8436e6c398cc998a70350bce4de3
Vytis Valentinavičius <xytis2000@gmail.com>
2022-11-22 14:02:24 +0200

    fix: python dir for new mac

--- nvim/lua/settings/python.lua
+++ nvim/lua/settings/python.lua
@@ -1,4 +1,4 @@
 vim.g.loaded_python_provider = 0
 -- Use brew location of python3 to circumvent direnv and localized pythons
 -- complaining about missing neovim package
-vim.g.python3_host_prog = "/usr/local/bin/python3"
+vim.g.python3_host_prog = "/opt/homebrew/bin/python3"

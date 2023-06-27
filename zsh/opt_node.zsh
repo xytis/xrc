b63fbc93fd91623d7808848eede72d5a4484abc8
Vytis Valentinavičius <xytis2000@gmail.com>
2023-06-27 11:57:31 +0300

    feat: removed nvm in favour of rtx

--- zsh/opt_node.zsh
+++
@@ -1,8 +0,0 @@
-# Check if brew based nvm is used
-NVMPATH=$(brew --prefix nvm)
-if [[ -d $NVMPATH ]]; then
-  NVM_DIR=$HOME/.nvm
-  [ -e $NVM_DIR ] || mkdir $NVM_DIR
-  [ -s "$NVMPATH/nvm.sh" ] && \. "$NVMPATH/nvm.sh" # This loads nvm
-  [ -s "$NVMPATH/bash_completion" ] && \. "$NVMPATH/bash_completion"  # This loads nvm bash_completion
-fi

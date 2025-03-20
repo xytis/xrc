705656fc27623d3715f0bee076fc6832d1b9abcf
Vytis Valentinavičius <xytis2000@gmail.com>
2025-03-20 12:18:15 +0200

    feat: fixed completion

---
+++ nvim/after/ftplugin/tf.lua
@@ -0,0 +1,9 @@
+require 'lspconfig'.terraformls.setup {}
+vim.api.nvim_create_autocmd({ "BufWritePre" }, {
+  pattern = { "*.tf", "*.tfvars" },
+  callback = function()
+    vim.lsp.buf.format()
+  end,
+})
+
+
--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -4,5 +4,15 @@ return {
     config = function()
     end
   },
-  'lukas-reineke/lsp-format.nvim'
+  'lukas-reineke/lsp-format.nvim',
+  {
+    "https://git.sr.ht/~whynothugo/lsp_lines.nvim",
+    config = function()
+      require("lsp_lines").setup()
+      vim.diagnostic.config({
+        virtual_text = false,
+        virtual_lines = true
+      })
+    end,
+  }
 }
